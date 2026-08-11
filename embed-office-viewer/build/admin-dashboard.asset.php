56bca556bcc4108fb6fb3b3805d30bc504107556
Sean Murphy <sean@iamseanmurphy.com>
2026-08-11 06:03:18 +0000

    2.4.2

--- embed-office-viewer/build/admin-dashboard.asset.php
+++ embed-office-viewer/build/admin-dashboard.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-data', 'wp-i18n'), 'version' => 'bc212b0ec36e7f505e3c');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-data', 'wp-i18n'), 'version' => 'bf7b4355410235f5494c');
